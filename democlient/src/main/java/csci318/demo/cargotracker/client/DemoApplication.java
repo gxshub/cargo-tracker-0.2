4b6e96f8fc93696563122c36dcbaf88694798e6a
guoxin <guoxin2009@gmail.com>
2023-09-19 21:00:44 +1000

    random booking requests

--- democlient/src/main/java/csci318/demo/cargotracker/client/DemoApplication.java
+++ democlient/src/main/java/csci318/demo/cargotracker/client/DemoApplication.java
@@ -5,41 +5,51 @@ import csci318.demo.cargotracker.client.dto.BookingIdDto;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	final String url = "http://localhost:8787/cargobooking";
+    final String url = "http://localhost:8787/cargobooking";
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return  args -> {
-			int i = 0;
-			while (i < 5) {
-				LocalDate day = LocalDate.now();
-				BookCargoResource entry = new BookCargoResource(10, "HK", "NY", day);
-				BookingIdDto bookingId = restTemplate.postForObject(url, entry, BookingIdDto.class);
-				System.out.println("******" + bookingId);
-				i++;
-				Thread.sleep(500);
-			}
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Random rand = new Random();
+            int upperbound = 24;
+            String[] cities = {"SYD", "SG", "HK", "NY"};
+            int numCities = cities.length;
+            LocalDate day = LocalDate.now();
+            //int i = 0;
+            while (true) {
+                int bookAmount = rand.nextInt(upperbound) + 1;
+                int r1 = rand.nextInt(numCities);
+                int r2 = rand.nextInt(numCities - 1);
+                String ori = cities[r1];
+                String des = cities[(r1 + r2 + 1) % numCities];
+                BookCargoResource entry = new BookCargoResource(bookAmount, ori, des, day);
+                BookingIdDto bookingId = restTemplate.postForObject(url, entry, BookingIdDto.class);
+                System.out.println("******" + bookingId + entry + "*****");
+                //i++;
+                Thread.sleep(500);
+            }
 
-		};
-	}
+        };
+    }
 
 
 }
