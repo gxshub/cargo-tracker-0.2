2cf01f30af16f8832b471007ec02e0cd0456c2f1
guoxin <guoxin2009@gmail.com>
2023-09-19 21:56:04 +1000

    delete code

--- analyticsms/src/main/java/csci318/demo/cargotracker/analyticsms/interfaces/rest/QueryController.java
+++
@@ -1,7 +0,0 @@
-package csci318.demo.cargotracker.analyticsms.interfaces.rest;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class QueryController {
-}
