ff899578adff523b3537963c94b32ae6563db6c3
guoxin <guoxin2009@gmail.com>
2023-09-19 21:48:43 +1000

    optimise code format

--- analyticsms/src/main/java/csci318/demo/cargotracker/analyticsms/applicationservice/StreamProcessor.java
+++ analyticsms/src/main/java/csci318/demo/cargotracker/analyticsms/applicationservice/StreamProcessor.java
@@ -1,15 +1,13 @@
 package csci318.demo.cargotracker.analyticsms.applicationservice;
 
 import csci318.demo.cargotracker.shareddomain.events.CargoBookedEvent;
-import csci318.demo.cargotracker.shareddomain.events.CargoBookedEventData;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.function.Consumer;
 
@@ -18,20 +16,21 @@ public class StreamProcessor {
     public final static String BOOKING_STATE_STORE = "cargo-booking";
 
     @Bean
-    public Consumer<KStream<String, CargoBookedEvent>> process(){
+    public Consumer<KStream<String, CargoBookedEvent>> process() {
         return inputStream -> {
 
-            //get RUNNING total booking amounts by destination
-            KStream<String, Long> aggregatedStream = inputStream.map((key,value) -> {
-                String destCity = value.getCargoBookedEventData().getDestLocation();
-                Long bookAmount = value.getCargoBookedEventData().getBookingAmount().longValue();
-                  return KeyValue.pair(destCity, bookAmount); }).
+            //generate RUNNING total booking amounts by destination
+            KStream<String, Long> aggregatedStream = inputStream.map((key, value) -> {
+                        String destCity = value.getCargoBookedEventData().getDestLocation();
+                        Long bookAmount = value.getCargoBookedEventData().getBookingAmount().longValue();
+                        return KeyValue.pair(destCity, bookAmount);
+                    }).
                     groupByKey(Grouped.with(Serdes.String(), Serdes.Long())).
                     reduce(Long::sum).toStream();
 
-           //just print the stream out to console
-           aggregatedStream.
-                   print(Printed.<String, Long>toSysOut().withLabel("Total booking amount by destination"));
+            //just print the stream out to console
+            aggregatedStream.
+                    print(Printed.<String, Long>toSysOut().withLabel("Total booking amount by destination"));
         };
     }
 }
